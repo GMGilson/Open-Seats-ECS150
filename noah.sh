cff74bb789d77188c10a81aa042823a5e48b5f81
Stephen Ott <stephenott531@gmail.com>
2021-05-10 15:35:22 -0700

    Improved one liner used to get number of seats

--- noah.sh
+++ noah.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+
 wget --quiet \
      --method POST \
      --header 'cache-control: no-cache' \
      --header 'content-type: application/x-www-form-urlencoded' \
      --body-data course_number="ECS150" \
      --output-document \
-     - https://registrar-apps.ucdavis.edu/courses/search/course_search_results.cfm | tee >(sed -rn '143~74p' | sed -r "s/<BR \/>//g" | cat -n) >(sed -rn '147~74p' | cat -n) >/dev/null | sort -n | cut -f2- | sed -r 's/^[[:space:]]*//g' | tail -n 1 | cat
+     - https://registrar-apps.ucdavis.edu/courses/search/course_search_results.cfm | awk 'BEGIN { p = 0 } p==1 && /[^.][0-9]+[[:space:]]*$/ { print $1; exit 0 } /<em>$/ { p = 1 }'
+
