c9ffe7eb19da45c0dc0138a8c63ad79c5b90605e
Grant Gilson <grantmaingilson@gmail.com>
2021-05-10 16:33:07 -0700

    incorporated stephens script

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.get("/shame", shame)
 
 function shame(request, res){
     const exec = require("child_process").exec;
-    exec(noah,
+    exec("./noah.sh",
         {shell: "/bin/zsh"},
         (err, stdout, stderr) => {
         console.log(`stdout: ${stdout}`)
@@ -24,8 +24,9 @@ function shame(request, res){
             res.send("Im a little teapot 418")
             return
         }
-        let seats = stdout.replace(/\s+/g,' ').trim();
-        seats = seats.split(" ").pop()
+        // let seats = stdout.replace(/\s+/g,' ').trim();
+        // seats = seats.split(" ").pop()
+        let seats = stdout
         res.send(JSON.stringify({openseats : seats}));
         return;
     })
@@ -35,4 +36,3 @@ const listener = app.listen(8080, () => {
     console.log(`Listening on port ${listener.address().port}`);
 })
 
-const noah = `wget --quiet --method POST --header 'cache-control: no-cache' --header 'content-type: application/x-www-form-urlencoded' --body-data course_number="ECS150" --output-document - https://registrar-apps.ucdavis.edu/courses/search/course_search_results.cfm | tee >(sed -n '143~74p' | sed -r "s/<BR \/>//g" | cat -n) >(sed -n '147~74p' | cat -n) >/dev/null | sort -n | cut -f2- | sed 's/^[[:space:]]*//g' | tail -n 1`
