2c6733991154dc80499e535d2bdf213cdf5df595
Grant Gilson <grantmaingilson@gmail.com>
2021-05-10 16:25:02 -0700

    reorganized promise chain in update.js

--- update.js
+++ update.js
@@ -4,10 +4,10 @@ setInterval(
     () => {
         fetch("/shame",{method: "GET"})
         .then((response) => {
-            response.json()
-            .then((body) => {
-                counter.innerHTML = `${body.openseats}`
-            })
             console.log(response)
-    });
-}, 10000);
+            return response.json();
+        })
+        .then((body) => {
+            counter.innerHTML = `${body.openseats}`
+        })
+}, 5000);
